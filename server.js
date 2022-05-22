7f0736f01d5744f9b756b458f8d10b13ab3e090d
zzivnuska <zzivnuska@gmail.com>
2022-05-22 04:42:37 -0700

    cleaning

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+console.log('greetings earthlings\nwe runnin shit...\n')
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+const users = {}
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
@@ -14,5 +18,5 @@ io.on('connection', (socket) => {
 });
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+  console.log(`Socket.IO server running on port ${port}`);
 });
