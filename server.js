f48ea8c0ff9855d842a5ed623781fc2f87e8c21f
zzivnuska <zzivnuska@gmail.com>
2022-05-23 20:39:09 -0700

    sleep deprived

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ console.log('greetings earthlings\nwe runnin shit...\n')
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+  console.log('user connected to site')
 });
 
 const users = {}
@@ -15,6 +16,7 @@ io.on('connection', (socket) => {
   socket.on('new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
+    console.log('user connected', name)
   })
 
   socket.on('chat message', message => {
@@ -24,6 +26,7 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
+    console.log('user disconnected')
   })
 });
 
